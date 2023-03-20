417e08356ecbe8cf55cbb72bfb5ee6f13ad4a988
Govardhan Reddy Morsu <govardhan.morsu@snaddevelopers.com>
2023-03-20 17:37:07 +0530

    docs updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import Docxtemplater from 'docxtemplater';
 import * as JSZip from 'jszip';
 import * as PizZip from 'pizzip';
 import { combineLatest } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -13,12 +15,79 @@ export class AppComponent implements OnInit {
   title = 'DocsGenerator';
   file: any;
   reader = new FileReader();
-  fileContent: string | ArrayBuffer | null = null;
+  fileContent: any | string | ArrayBuffer | null = null;
+  myForm!: FormGroup;
 
-  constructor() {}
+  constructor(private http: HttpClient, private formBuilder: FormBuilder) {
+    this.myForm = this.formBuilder.group({
+      name: ['', Validators.required],
+
+      line1: ['', Validators.required],
+      line2: ['', Validators.required],
+      city: ['', Validators.required],
+      pincode: ['', Validators.required],
+      salary: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(/^\$?[0-9]+(\.[0-9]{1,2})?$/),
+        ]),
+      ],
+      issueddate: ['', Validators.required],
+      lastdate: ['', Validators.required],
+      worklocation: ['', Validators.required],
+      Issuername: ['', Validators.required],
+      Designation: ['', Validators.required],
+    });
+  }
 
   ngOnInit(): void {
     // this.getDocs();
+
+    this.getOfferletter();
+  }
+
+  async getOfferletter() {
+    this.fileContent = await this.http
+      .get('assets/offer.docx', {
+        responseType: 'arraybuffer',
+      })
+      .toPromise();
+  }
+
+  async onSubmit() {
+    if (this.myForm.valid) {
+      const data = this.myForm.value;
+      data.issueddate =  this.formatDate(data.issueddate)
+      data.lastdate =  this.formatDate(data.lastdate)
+
+      console.log(data);
+      // call function to generate document with data
+
+      const templateFile = await fetch('assets/offer.docx');
+      const templateBlob = await templateFile.blob();
+
+      const doc = new Docxtemplater().loadZip(new PizZip(this.fileContent));
+
+      // do something with the doc...
+
+      doc.setData(data);
+      let result = doc.render();
+      console.log(result);
+
+      const output = doc.getZip().generate({
+        type: 'blob',
+        mimeType:
+          'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+      });
+      // fs.writeFileSync('Desktop/output_document.docx', output);
+      console.log(output);
+      // const blob = this.base64ToBlob(output);
+      const link = document.createElement('a');
+      link.href = URL.createObjectURL(output);
+      link.download = `${data.name}_offerletter.docx`;
+      link.click();
+    }
   }
 
   private formatDate(dateString: Date): string {
@@ -41,62 +110,42 @@ export class AppComponent implements OnInit {
     if (!this.fileContent) {
       return;
     }
+    const templateFile = await fetch('assets/offer.docx');
+    const templateBlob = await templateFile.blob();
 
+    const doc = new Docxtemplater().loadZip(new PizZip(this.fileContent));
 
-      const doc = new Docxtemplater().loadZip(new PizZip(this.fileContent));
-      // do something with the doc...
-      const data = {
-        name: 'John Doe',
-        address: '123 Main Street',
-        salary: '$100,000',
-        issueddate: this.formatDate(new Date()),
-        lastdate: this.formatDate(new Date()),
-        worklocation: 'Hyderabad',
-        Issuername: 'Mary',
-        // add other dynamic fields here
-      };
-
-
-
-
-      doc.setData(data);
-      let result = doc.render();
-      console.log(result);
-
-      const output = doc.getZip().generate({ type: 'nodebuffer' });
-      // fs.writeFileSync('Desktop/output_document.docx', output);
-      console.log(output);
-
-  }
-
-  getDocs(content: File) {
-    // const content = fs.readFileSync('Desktop/offer.docx', 'binary');
-
-    this.reader.readAsDataURL(content);
-    this.reader.onload = () => {
-      const fileContent = this.reader.result as string;
-      // use the file content to insert into the template
-      this.file = {
-        myFileVariable: fileContent,
-      };
-    };
-    const doc = new Docxtemplater();
-
+    // do something with the doc...
     const data = {
-      name: 'John Doe',
-      address: '123 Main Street',
-      salary: '$100,000',
+      name: 'Govardhan Reddy',
+      line1: '4-79, Markook',
+      line2: 'Markook',
+      city: 'Siddipet',
+      pincode: 502279,
+      salary: '1,00,000',
       issueddate: this.formatDate(new Date()),
       lastdate: this.formatDate(new Date()),
       worklocation: 'Hyderabad',
-      Issuername: 'Mary',
+      Issuername: 'Ravi Teja',
+      Designation: 'SDE',
       // add other dynamic fields here
     };
 
     doc.setData(data);
-    doc.render();
-    const output = doc.getZip().generate({ type: 'nodebuffer' });
+    let result = doc.render();
+    console.log(result);
+
+    const output = doc.getZip().generate({
+      type: 'blob',
+      mimeType:
+        'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+    });
     // fs.writeFileSync('Desktop/output_document.docx', output);
     console.log(output);
+    // const blob = this.base64ToBlob(output);
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(output);
+    link.download = `${data.name}_offerletter.docx`;
+    link.click();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,13 +8,18 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+
+    ReactiveFormsModule
+    ],
+  providers: [
+
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
